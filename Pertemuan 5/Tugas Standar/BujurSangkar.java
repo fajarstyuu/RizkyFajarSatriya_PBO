52c8fed3eb72a8fb1183f6962a885a2aecbf9f3f
fajarstyuu <96579944+fajarstyuu@users.noreply.github.com>
2024-03-23 22:09:32 +0700

    Update BujurSangkar.java

--- Pertemuan 5/Tugas Standar/BujurSangkar.java
+++ Pertemuan 5/Tugas Standar/BujurSangkar.java
@@ -1,3 +1,7 @@
+// Nama     : Rizky Fajar Satriya
+// NIM      : 24060122120017
+// Deskripsi: Kelas Driver
+// Tanggal  : 23/03/2024
 public class BujurSangkar extends BangunDatar {
     public double hitungLuas(double sisi){
         luas = sisi*sisi;
